27f27bf39ef8d14b13a7017b3bcf9a8c4c633654
KareemErgawy <kareem.ergawy@gmail.com>
2018-08-31 20:33:23 +0200

    Some examples from C++ Templates: The Complete Guide
     - cref.

---
+++ c++-templates-complete-guide/basics/cref.cpp
@@ -0,0 +1,15 @@
+#include <functional>
+#include <iostream>
+#include <string>
+
+void printString(std::string const &s) { std::cout << s << '\n'; }
+
+template <typename T> void printT(T arg) { printString(arg); }
+
+int main() {
+  std::string s = "hello";
+  printT(s);
+  printT(std::cref(s));
+
+  return 0;
+}
